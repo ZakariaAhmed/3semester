184388aa1213058403368a92af97d7b5518e2f48
zakariaahmed <zackyahmed89@gmail.com>
2018-02-28 10:32:31 +0100

    some of them

---
+++ startCodeForThreadExercises/src/main/java/day2/deadlock/DeadLockDetector.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2.deadlock;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+/**
+ *
+ * @author zacky
+ */
+public class DeadLockDetector extends Thread{
+     ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+     
+      public void run()
+    {
+        while (true)
+        {
+            long[] threadIds = bean.findDeadlockedThreads();
+            if (threadIds != null)
+            {
+                System.out.println("Deadlock detected " + threadIds.length);                
+            }
+        }
+    }
+}
